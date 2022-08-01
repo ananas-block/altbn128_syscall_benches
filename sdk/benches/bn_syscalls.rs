e546aabd74175e6c7c24df1bf292bc9bc6bd4e60
ananas-block <flow23@protonmail.com>
2022-08-02 00:11:11 +0100

    added addition bench

--- sdk/benches/bn_syscalls.rs
+++ sdk/benches/bn_syscalls.rs
@@ -29,111 +29,18 @@ type G2 = ark_ec::short_weierstrass_jacobian::GroupAffine::<ark_bn254::g2::Param
 
 type G1 = ark_ec::short_weierstrass_jacobian::GroupAffine::<ark_bn254::g1::Parameters>;
 
-#[bench]
-fn alt_bn128_addition_test(b: &mut Bencher) {
-    use serde::Deserialize;
-    use std::time::Instant;
-    let TEST_DATA = r#"[
-        {
-            "Input": "18b18acfb4c2c30276db5411368e7185b311dd124691610c5d3b74034e093dc9063c909c4720840cb5134cb9f59fa749755796819658d32efc0d288198f3726607c2b7f58a84bd6145f00c9c2bc0bb1a187f20ff2c92963a88019e7c6a014eed06614e20c147e940f2d70da3f74c9a17df361706a4485c742bd6788478fa17d7",
-            "Expected": "2243525c5efd4b9c3d3c45ac0ca3fe4dd85e830a4ce6b65fa1eeaee202839703301d1d33be6da8e509df21cc35964723180eed7532537db9ae5e7d48f195c915",
-            "Name": "chfast1",
-            "Gas": 150,
-            "NoBenchmark": false
-        }
-    ]"#;
+fn alt_bn128_addition_test_rnd(c: &mut Criterion) {
+    let mut rng = test_rng();
+    // let mut input = Vec::new();
 
-    #[derive(Deserialize)]
-    #[serde(rename_all = "PascalCase")]
-    struct TestCase {
-        input: String,
-        expected: String,
-        name: String,
-    }
+    let input = [&to_be_64(&get_random_g1(&mut rng))[..], &to_be_64(&get_random_g1(&mut rng))[..]].concat();
+    let mut i = 0;
+    c.bench_function("addition rnd", |b| b.iter(|| {
+        alt_bn128_addition(&input);
+        i+=1;
+    }));
 
-    let test_cases: Vec<TestCase> = serde_json::from_str(TEST_DATA).unwrap();
 
-    b.iter(|| {
-
-        test_cases.iter().for_each(|test| {
-
-            let input = array_bytes::hex2bytes_unchecked(&test.input);
-
-            let result = alt_bn128_addition(&input);
-            assert!(result.is_ok());
-            let result = result.unwrap();
-
-            let expected = array_bytes::hex2bytes_unchecked(&test.expected);
-            assert_eq!(result, expected);
-        });
-    });
-}
-
-#[bench]
-fn alt_bn128_multiplication_test(b: &mut Bencher) {
-    use serde::Deserialize;
-
-    let TEST_DATA = r#"[
-        {
-            "Input": "2bd3e6d0f3b142924f5ca7b49ce5b9d54c4703d7ae5648e61d02268b1a0a9fb721611ce0a6af85915e2f1d70300909ce2e49dfad4a4619c8390cae66cefdb20400000000000000000000000000000000000000000000000011138ce750fa15c2",
-            "Expected": "070a8d6a982153cae4be29d434e8faef8a47b274a053f5a4ee2a6c9c13c31e5c031b8ce914eba3a9ffb989f9cdd5b0f01943074bf4f0f315690ec3cec6981afc",
-            "Name": "chfast1",
-            "Gas": 6000,
-            "NoBenchmark": false
-        },{
-            "Input": "070a8d6a982153cae4be29d434e8faef8a47b274a053f5a4ee2a6c9c13c31e5c031b8ce914eba3a9ffb989f9cdd5b0f01943074bf4f0f315690ec3cec6981afc30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd46",
-            "Expected": "025a6f4181d2b4ea8b724290ffb40156eb0adb514c688556eb79cdea0752c2bb2eff3f31dea215f1eb86023a133a996eb6300b44da664d64251d05381bb8a02e",
-            "Name": "chfast2",
-            "Gas": 6000,
-            "NoBenchmark": false
-        },{
-            "Input": "025a6f4181d2b4ea8b724290ffb40156eb0adb514c688556eb79cdea0752c2bb2eff3f31dea215f1eb86023a133a996eb6300b44da664d64251d05381bb8a02e183227397098d014dc2822db40c0ac2ecbc0b548b438e5469e10460b6c3e7ea3",
-            "Expected": "14789d0d4a730b354403b5fac948113739e276c23e0258d8596ee72f9cd9d3230af18a63153e0ec25ff9f2951dd3fa90ed0197bfef6e2a1a62b5095b9d2b4a27",
-            "Name": "chfast3",
-            "Gas": 6000,
-            "NoBenchmark": false
-        },{
-            "Input": "1a87b0584ce92f4593d161480614f2989035225609f08058ccfa3d0f940febe31a2f3c951f6dadcc7ee9007dff81504b0fcd6d7cf59996efdc33d92bf7f9f8f6ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-            "Expected": "2cde5879ba6f13c0b5aa4ef627f159a3347df9722efce88a9afbb20b763b4c411aa7e43076f6aee272755a7f9b84832e71559ba0d2e0b17d5f9f01755e5b0d11",
-            "Name": "cdetrio1",
-            "Gas": 6000,
-            "NoBenchmark": false
-        },{
-            "Input": "17c139df0efee0f766bc0204762b774362e4ded88953a39ce849a8a7fa163fa901e0559bacb160664764a357af8a9fe70baa9258e0b959273ffc5718c6d4cc7cffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-            "Expected": "29e587aadd7c06722aabba753017c093f70ba7eb1f1c0104ec0564e7e3e21f6022b1143f6a41008e7755c71c3d00b6b915d386de21783ef590486d8afa8453b1",
-            "Name": "cdetrio6",
-            "Gas": 6000,
-            "NoBenchmark": false
-        },{
-            "Input": "039730ea8dff1254c0fee9c0ea777d29a9c710b7e616683f194f18c43b43b869073a5ffcc6fc7a28c30723d6e58ce577356982d65b833a5a5c15bf9024b43d98ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-            "Expected": "00a1a234d08efaa2616607e31eca1980128b00b415c845ff25bba3afcb81dc00242077290ed33906aeb8e42fd98c41bcb9057ba03421af3f2d08cfc441186024",
-            "Name": "cdetrio11",
-            "Gas": 6000,
-            "NoBenchmark": false
-        }
-    ]"#;
-
-    #[derive(Deserialize)]
-    #[serde(rename_all = "PascalCase")]
-    struct TestCase {
-        input: String,
-        expected: String,
-    }
-
-    let test_cases: Vec<TestCase> = serde_json::from_str(TEST_DATA).unwrap();
-    println!("test_cases len: {:?}",test_cases.len() );
-    b.iter(|| {
-
-        test_cases.iter().for_each(|test| {
-            let input = array_bytes::hex2bytes_unchecked(&test.input);
-            let result = alt_bn128_multiplication(&input);
-            assert!(result.is_ok());
-            let result = result.unwrap();
-
-            let expected = array_bytes::hex2bytes_unchecked(&test.expected);
-            assert_eq!(result, expected);
-        });
-    });
 }
 
 fn get_random_g1(rng: &mut StdRng) -> [u8;64] {
@@ -198,7 +105,8 @@ criterion_group!(benches,
     alt_bn128_pairing_test_2,
     // // alt_bn128_pairing_test_rnd,
     alt_bn128_pairing_test_rnd_2,
-    alt_bn128_multiplication_test_rnd
+    alt_bn128_multiplication_test_rnd,
+    alt_bn128_addition_test_rnd
 );
 criterion_main!(benches);
 
